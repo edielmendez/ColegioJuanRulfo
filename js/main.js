832de76d8d3f1751b8d86004eac75891e69971ca
edielmendez <you@example.com>
2018-10-15 22:52:49 -0500

    se agregaron los videos

--- js/main.js
+++ js/main.js
@@ -15,7 +15,30 @@ $(document).on("click","#btn_form",function(){
 	}
 });
 
-$(document).on("click",".testimony_title",function(){
+
+$(document).on("click","#testimony_one",function(){
+	document.getElementById("video_testimonio").src = "video/testimonio.mp4";
 	
-	$('#modalVideo').modal()
-});
+	$("#modalVideo").modal();
+})
+
+$(document).on("click","#testimony_two",function(){
+	document.getElementById("video_testimonio").src = "video/testimonio2.mp4";
+	$("#modalVideo").modal();
+})
+
+
+$('#modalVideo').on('hidden.bs.modal', function (e) {
+ 
+  document.getElementById("video_testimonio").pause();
+
+  
+})
+
+$(document).on("click","#img_collapse",function(e){
+	
+	var src = ($(this).attr('src') === 'images/flecha3.png') ? 'images/flecha5.png' : 'images/flecha3.png';
+	$(this).attr('src', src);
+         
+})
+
